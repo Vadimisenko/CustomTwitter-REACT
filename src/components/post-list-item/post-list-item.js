8af16990b5f35dd80d030eb0f6a71905483763c4
Vadym Isenko <vigmail@ukr.net>
2021-07-27 19:49:52 +0300

    Initial

---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+
+import './post-list-item.sass'
+
+export default class PostListItem extends Component {
+
+  
+
+    render() {
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
+
+        let classNames = 'app-list-item d-flex justify-content-between';
+        if (important) {
+            classNames += ' important';
+        }
+        if (like) {
+            classNames += ' like';
+        }
+
+        return(
+            <div className={classNames}>
+                <span 
+                    onClick= {onToggleLiked}
+                    className="app-list-item-label"> 
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                        type = "button"
+                        onClick={onToggleImportant}    
+                        className="btn-star btn-sm">
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button 
+                        type = "button"
+                        className="btn-trash btn-sm"
+                        onClick= {onDelete}>
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
+            </div>
+        )
+    }
+}
